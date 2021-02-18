58858e22db73274170bee71a3a90ee918d375436
josephjerryrhule <josephjerryrhule@gmail.com>
2021-02-18 22:00:24 +0000

    Package Installations

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCBbBViI7dfJdGA9zn5oCMP2CFs5FJJWdc",
+    authDomain: "signal-app-clone-b6a35.firebaseapp.com",
+    projectId: "signal-app-clone-b6a35",
+    storageBucket: "signal-app-clone-b6a35.appspot.com",
+    messagingSenderId: "1023287164355",
+    appId: "1:1023287164355:web:c754336169b6456fca5176",
+    measurementId: "G-5P9E0LDG64"
+  };
