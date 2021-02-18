8d9e19eaa1c15c4e2d357041c79c43753ff7c762
josephjerryrhule <josephjerryrhule@gmail.com>
2021-02-18 22:17:45 +0000

    Initialized LoginScreen

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "./screens/LoginScreen";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Stack.Screen name="Login" component={LoginScreen} />
       <View style={styles.container}>
-       <StatusBar style="auto" />
-       <Text>Open up App.js to start working on your app!</Text>
-    </View>
+        <StatusBar style="auto" />
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
     </NavigationContainer>
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
