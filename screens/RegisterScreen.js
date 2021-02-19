5926886dbc14a8886a87c44c1c70ff3c3e06e46c
josephjerryrhule <josephjerryrhule@gmail.com>
2021-02-19 00:33:18 +0000

    Introduction of useLayoutEffect

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, KeyboardAvoidingView } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import { StyleSheet, View, KeyboardAvoidingView } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Button, Input } from "react-native-elements";
+import { Button, Input, Text } from "react-native-elements";
 
 const RegisterScreen = ({ navigation }) => {
   const [name, setName] = useState("");
@@ -9,6 +9,13 @@ const RegisterScreen = ({ navigation }) => {
   const [password, setPassword] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const register = () => {};
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackTitle: "Back to Login",
+    });
+  }, [navigation]);
+
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <StatusBar style="light" />
@@ -17,7 +24,7 @@ const RegisterScreen = ({ navigation }) => {
         Create a Signal account
       </Text>
 
-      <View style={styles.container}>
+      <View style={styles.inputContainer}>
         <Input
           placeholder="Full Name"
           autoFocus
@@ -49,6 +56,14 @@ const RegisterScreen = ({ navigation }) => {
           onSubmitEditing={register}
         />
       </View>
+
+      <Button
+        containerStyle={styles.button}
+        raised
+        onPress={register}
+        title="Register"
+      />
+      <View style={{ height: 100 }} />
     </KeyboardAvoidingView>
   );
 };
@@ -56,5 +71,18 @@ const RegisterScreen = ({ navigation }) => {
 export default RegisterScreen;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+  inputContainer: {
+    width: 300,
+  },
 });
