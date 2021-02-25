b0dd9f78f9d2ff0c8645167b4378282f1e14356b
Joseph <josephjerryrhule@gmail.com>
2021-02-25 18:01:49 +0000

    Fixed alert bug

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>The Home Screen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
